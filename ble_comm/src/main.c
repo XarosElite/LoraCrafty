f5100b03a643686c96f5b7fa51c6617c0ebb68f6
Nathan Dunn <ndunn2230@gmail.com>
2025-10-09 21:56:02 -0400

    Basic input reading for mode selection

--- ble_comm/src/main.c
+++ ble_comm/src/main.c
@@ -8,8 +8,35 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
+#include "main.h"
+
+enum APP_MODE read_app_mode(void) {
+    char buffer[32];
+    int number;
+
+    while(true) {
+        printf("What mode? (0) Send, (1) Receive: ");
+
+        if (fgets(buffer, sizeof(buffer), stdin)) {
+            if (
+                sscanf(buffer, "%d", &number) == 1 &&
+                (number == APP_RECV || number == APP_SEND)
+            ) {
+                return (enum APP_MODE) number;
+            } else {
+                printf("Invalid mode.\n");
+            }
+        }
+    }
+}
+
 int main(void) {
-    printf("Hello from ble_comm\n");
-    
+
+    enum APP_MODE mode;
+
+    mode = read_app_mode();
+
+    printf("You entered: %d\n", mode);
+
     return 0;
 }
---
+++ ble_comm/src/main.h
@@ -0,0 +1,8 @@
+
+#define true 1
+#define false 0
+
+enum APP_MODE {
+    APP_RECV,
+    APP_SEND
+};
