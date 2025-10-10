533aafa3f319b83b56af1f8a379ef54839648b83
Jacob Poling <polingj999@gmail.com>
2025-10-09 22:48:21 -0400

    I'm rusting it...I'm sorry

---
+++ loracord-rs/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "loracord dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              rust-bin.beta.latest.default
+              rust-analyzer
+              ra-multiplex
+              pkg-config
+              openssl
+            ];
+          };
+      }
+    );
+}
